f5cc230f7b2977c52e949629c67770204ea0b705
guidomeo <guido.meo@gmail.com>
2021-08-19 19:48:03 +0200

    added fullscreen for video

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ function getAge(dateString) {
     return age;
 }
 
+function openFullscreen(myVideo) {
+    var elem = myVideo;
+    console.log(elem);
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }
+
 const age = document.getElementById('age');
 age.innerHTML = getAge("03/31/1996");
 
@@ -20,4 +34,10 @@ fitText(document.querySelector("h1"), 0.8);
 fitText(document.querySelector("h2"), 1.2);
 document.querySelectorAll("h3").forEach(element => {
     fitText(element, 1.4);
+});
+
+document.querySelectorAll("video").forEach(element => {
+    element.addEventListener('click', function(e) {
+        openFullscreen(element);
+    }, false);
 });
